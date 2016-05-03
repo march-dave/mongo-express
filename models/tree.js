8a05d69c12bad932bfe198f99301af2b49f8799c
march-dave <ysangy@gmail.com>
2016-05-03 16:47:21 -0700

    nest dimensation insert update

--- models/tree.js
+++ models/tree.js
@@ -2,9 +2,32 @@
 
 var mongoose = require('mongoose');
 
-var Tree = mongoose.model('Tree', {
-  species: String,
-  age: Number
+var treeSchema = new mongoose.Schema({
+  // species: { type: String, required: true, default: pickAPet },
+  // species: { type: String, required: true, enum: ['sequoia', 'ash', 'pine', 'cherry'] },
+  // createdAt: { type:Date, default: Date.now },
+  species: { type: String },
+  createdAt: { type:Date },
+  // age: { type: Number, required: true, min: 0, max: 1000 }
+  // age: { type: Number, required: true, enum: [1,2,3,4,5] }
+  // age: { type: Number, required: true },
+  age: { type: Number },
+  creatures: [{ type: String }] // , match: /^\w+/ an array of strings
+
+
 });
+
+var Tree = mongoose.model('Tree', treeSchema);
+
+// function pickAPet() {
+//   var pets = ['sequoia', 'ash', 'pine', 'cherry'];
+//   var randIndex = Math.floor(Math.random() * pets.length);
+//   return pets[randIndex];
+// }
+
+// var Tree = mongoose.model('Tree', {
+//   species: String,
+//   age: Number
+// });
 
 module.exports = Tree;
--- routes/trees.js
+++ routes/trees.js
@@ -4,70 +4,111 @@ var router = express.Router();
 var Tree = require('../models/tree');
 
 
-// GET /api/trees   ===> return array of all trees
-// GET /api/trees/id  ===> return one tree by id
-// POST /api/trees  ===> create a new tress
+router.route('/')
+  .get((req, res) => {
+
+    // .sort() sort the results
+    // .limit limit the numer of results
+    // .select() limit the fields that will be in the results
+
+    // paging
+    var page = parseInt(req.query.page) || 1;
 
 
-/* GET users listing. */
-router.get('/', (req, res) => {
-  Tree.find({}, (err, trees) => {
-    if(err) {
-      res.status(400).send(err);
-    } else {
-      res.send(trees);
-    }
+    // Tree.find({}, (err, trees) => {
+    Tree
+      .find({ age: {$lt: 30}})
+      .skip( page * 20 - 20 )
+      // .select( { species: 1, age: 1, _id: 0 } )
+      .select( { } )
+      .limit(req.query.limit)
+      .sort({age: -1})
+      .exec((err, trees) => {
+
+
+      res.status(err ? 400 : 200).send(err || trees);
+
+
+    });
+
+
+
+
+  })
+  .post((req, res) => {
+    var tree = new Tree(req.body);
+
+    console.log('tree:', tree);
+
+    tree.save((err, savedTree) => {
+      res.status(err ? 400 : 200).send(err || savedTree);
+    });
+  })
+
+router.route('/:id')
+  .get((req, res) => {
+    Tree.findById(req.params.id, (err, tree) => {
+      res.status(err ? 400 : 200).send(err || tree);
+    });
+  })
+  .delete((req, res) => {
+    Tree.findByIdAndRemove(req.params.id, (err, tree) => {
+      res.status(err ? 400 : 200).send(err);
+    });
+  })
+  .put((req, res) => {
+    Tree.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true }, (err, tree) => {
+      res.status(err ? 400 : 200).send(err || tree);
+    });
+  })
+
+
+router.put('/:id/:creature', (req, res) =>  {
+
+  Tree.findById(req.params.id, (err, tree) => {
+    if(err) return res.status(400).send(see);
+
+    // var newCreature = req.body.creature;
+    var newCreature = req.params.creature;
+    // var newCreature = req.params.creature;
+    console.log('req.body.creature: ', newCreature);
+    tree.creatures.push(newCreature);
+
+    tree.save((err, savedTree) => {
+      res.status(err ? 400: 200).send(err|| savedTree);
+    });
+
+
+    // tree.save( function(err, savedTree) {
+    //   res.status(err ? 400: 200).send(err|| savedTree);
+    // });
+
   });
 });
 
-/* GET /api/trees/:id */
-router.get('/:id', (req, res) => {
+router.delete('/:id/:creature', (req, res) => {
 
-  Tree.findById(req.params.id, (err, trees) => {
-    if(err) {
-      res.status(400).send(err);
-    } else {
-      res.send(trees);
-    }
-  });
-});
+  Tree.findById(req.params.id, (err, tree) => {
+    if(err) return res.status(400).send(err);
 
-router.delete('/:id', (req, res) => {
-  Tree.findByIdAndRemove(req.params.id, (err, trees) => {
-    if(err) {
-      res.status(400).send(err);
-    } else {
-      res.send();
-    }
+    var index = tree.creatures.indexOf(req.params.creature);
+    tree.creatures.splice(index, 1);
+
+    tree.save((err, savedTree) => {
+      res.status(err ? 400: 200).send(err || savedTree);
+    });
+
   });
 
 });
 
-router.put('/:id', (req, res) => {
 
-  // req.params.id --> docuemt id
-  // req.body --> update obj
+// route.rotue('/nest/:id')
+//   .get((req, res) => {
+//     //
+//   })
+  // .put((req, res ))
 
-  Tree.findByIdAndUpdate(req.params.id,  {$set: req.body},  {new: true}, (err, tree) => {
-    if(err) {
-      res.status(400).send(err);
-    } else {
-      res.send(tree);
-    }
-  });
-});
-
-router.post('/', (req, res) => {
-  var tree = new Tree(req.body);
-  tree.save((err, savedTree) => {
-
-    if(err) {
-      res.status(400).send(err);
-    } else {
-      res.send('savedTree');
-    }
-  });
-});
 
 
 module.exports = router;
